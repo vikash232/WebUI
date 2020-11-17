dc2fa45cf8f2bf5c568e5e0afd4c507c74c6f2fd
VIKASH KAUSHIK <58074696+vikash232@users.noreply.github.com>
2020-11-17 23:48:46 +0530

    linuxall.py

---
+++ linuxall.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import cgi
+print("content-type: text/html")
+print()
+
+form = cgi.FieldStorage()
+cmd = form.getvalue("x")
+
+print(cmd)
+
+import subprocess
+
+x=subprocess.getoutput(cmd)
+print(x)
